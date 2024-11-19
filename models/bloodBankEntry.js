3ae0fd1e385bfbe9cc357fb97c71443158d0ebe0
Srinidhi Krishnan <kadalazhisrinidhi2103@gmail.com>
2024-11-19 17:31:50 +0530

    DB schema

---
+++ models/bloodBankEntry.js
@@ -0,0 +1,13 @@
+class BloodBankEntry{
+    constructor(id,name,age,bldgrp,contact,qty,collectionDate,expirationDate,status){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.bldgrp = bldgrp;
+        this.contact = contact;
+        this.qty = qty;
+        this.collectionDate = collectionDate;
+        this.expirationDate = expirationDate;
+        this.status = status;
+    }
+}
